867f7223251dc01025524a96e4a28b04ee9fbcdd
breadman22 <42971506+ThatGuyBreadman1567@users.noreply.github.com>
2018-11-01 10:52:42 -0400

    Update PizzaOrder.py

--- PizzaOrder.py
+++ PizzaOrder.py
@@ -1,7 +1,26 @@
 #Pizza Order
 #Benjamin Readman
-print("Wlecome to Pizza Pie People!")
+print("Welcome to Pizza Pie People!")
 name=str(input("What is your name?"))
 gf=str(input("Would you like to have a gluten free pizza?"))
+price=12.50
 if gf == 'yes':
+  price=14.50
+  pizza=str(input("Would you like to have pepperoni on that pizza:"))
+  if pizza == 'yes':
+    price+=1.25
+    print('Thank you, ',name,", that pizza is $",price,sep=(""))
+  else:
+    if pizza == 'no':
+      print('Thank you, ',name,", that pizza is $",price,sep=(""))
+else:
+  if gf == 'no':
     pizza=str(input("Would you like to have pepperoni on that pizza:"))
+    if pizza == 'yes':
+      price+=1.25
+      print('Thank you, ',name,", that pizza is $",price,sep=(""))
+    else:
+      if pizza == 'no':
+        print('Thank you, ',name,", that pizza is $",price,sep=(""))
+  else:
+    print("Sorry, I didn't get that, GOODBYE")
