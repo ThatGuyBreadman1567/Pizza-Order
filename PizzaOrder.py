a216496a6c549b03999a6813c215db5808f30fb8
breadman22 <42971506+ThatGuyBreadman1567@users.noreply.github.com>
2018-11-01 10:24:42 -0400

    Add files via upload

---
+++ PizzaOrder.py
@@ -0,0 +1,7 @@
+#Pizza Order
+#Benjamin Readman
+print("Wlecome to Pizza Pie People!")
+name=str(input("What is your name?"))
+gf=str(input("Would you like to have a gluten free pizza?"))
+if gf == 'yes':
+    pizza=str(input("Would you like to have pepperoni on that pizza:"))
